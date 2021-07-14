fda1a23dfa8435f1bfea15e089450d1dc6c26c9e
Matt Dailey <mattddailey@gmail.com>
2021-07-13 22:07:30 -0700

    Barebones Youtube React Mock

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 
-const API_KEY = 'API_KEY_HERE';
+const API_KEY = 'AIzaSyDY3p9EU1UxW3fC3DpaixCssQ7PqG0kAkw';
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+class App extends Component {
+    constructor(props) {
+        super(props);
+  
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+        
+        this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+  
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList 
+                    onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
 }
-
-
-ReactDOM.render(<App />, document.querySelector('.container'));
+  
+ReactDOM.render(<App />, document.querySelector(".container"));
+  
